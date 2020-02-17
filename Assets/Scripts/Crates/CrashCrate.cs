094755a0cb32b1840ddd465c5b54811a983d3f75
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-02-17 16:18:16 +0100

    Add crash crate (add 1 heart) and basic crate ( add x wumpas).

---
+++ Assets/Scripts/Crates/CrashCrate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashCrate : MonoBehaviour
+{
+    //Reference to animator
+    private Animator animator;
+
+    //Reference to platform layer
+    private int playerLayer;
+    
+    //Reference to tornado attack
+    private TornadoAttack AttackManager;
+    
+    //Reference to playerhealth
+    private PlayerHealth playerHealth;
+    
+    private void Awake()
+    {
+        Debug.Log("I'm awake");
+        playerLayer = LayerMask.NameToLayer("Player");
+        animator = GetComponent<Animator> ();
+        AttackManager = GameObject.Find("Crash").GetComponent<TornadoAttack>();
+        playerHealth = GameObject.Find("Crash").GetComponent<PlayerHealth>();
+    }
+
+    void DestroyCrate(Collider2D collision)
+    {
+        if (collision.gameObject.layer == playerLayer & AttackManager.isAttacking)
+        {
+            Debug.Log("Got destroyed");
+            animator.SetBool("isDestroyed", true);
+        }
+    }
+
+    void DestroyAnimationOver()
+    {
+        Debug.Log("AnimationOver");
+        animator.SetBool("isDestroyed", false);
+        playerHealth.remainingLives += 1;
+        playerHealth.refreshUI();
+        Destroy(gameObject);
+    }
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        DestroyCrate(collision);
+    }
+}
---
+++ Assets/Scripts/Crates/WumpaCrate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WumpaCrate : MonoBehaviour
+{
+    //Get prefab
+    public GameObject wumpaPrefab;
+    
+    //Reference to animator
+    private Animator animator;
+
+    //Reference to platform layer
+    private int playerLayer;
+    
+    //Reference to tornado attack
+    private TornadoAttack AttackManager;
+    
+    private void Awake()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+        animator = GetComponent<Animator> ();
+        AttackManager = GameObject.Find("Crash").GetComponent<TornadoAttack>();
+    }
+
+    void DestroyCrate(Collider2D collision)
+    {
+        if (collision.gameObject.layer == playerLayer & AttackManager.isAttacking)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(wumpaPrefab, transform.position, transform.rotation);
+            }
+            animator.SetBool("isDestroyed", true);
+        }
+    }
+
+    void DestroyAnimationOver()
+    {
+        animator.SetBool("isDestroyed", false);
+        Destroy(gameObject);
+    }
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        DestroyCrate(collision);
+    }
+}
