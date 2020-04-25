0ac73e027cfbf4fb5d7f21e591589a7bc40cef7e
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-25 21:20:36 +0200

    Add crab animation and patrol movement

---
+++ Assets/Scripts/Enemies/PatrolMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolMovement : PhysicsObject
+{
+    public float speed = 1;
+    public bool moveRight;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("turn"))
+        {
+            if (moveRight)
+            {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+        }
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        if (moveRight)
+        {
+            move.x = 1;
+        }
+        else
+        {
+            move.x = -1;
+        }
+        targetVelocity = move * speed;
+    }
+}
